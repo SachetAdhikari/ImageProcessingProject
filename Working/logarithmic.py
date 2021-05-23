9919c532f4114bd6c56511e1a11d79bd7afb5e60
SachetAdhikari <sachet.adhi@gmail.com>
2021-05-23 20:45:55 +0545

    update logarithm and practise.py

--- Working/logarithmic.py
+++ Working/logarithmic.py
@@ -1,33 +1,19 @@
-import math
+from math import log10
 from PIL import Image
 from numpy import array
-
-img = Image.open('..\\InputImages\\lena.png')
+img = Image.open("..\\InputImages\\lena.png")
 arr = array(img)
-print(arr)
-# arr=arr.transpose(2,0,1).reshape(-1,arr.shape[1])
+img.close()
 if arr.ndim > 2:
     arr = arr[:, :, 0]
-print(arr)
-# 1kbcfsdfb72184t82bsdkmb dhsfbnk=arr;#nk=[[8,5,8,8,5,8],[8,5,8,8,5,8],[8,5,8,8,5,8],[8,5,8,8,5,8],[8,5,8,8,5,8],[8,5,8,8, 5,8]];
-# for i in range(len(nk)):
-# print(*nk[i])
-x = max(map(max, nk))
-m = len(nk[1])
-n = len(nk)
-print(m, n)
-opnk = nk
-x = 0
-#A=int(input("enter constaant"));
-# f = open('log.txt')
-# read = f.readline().split() print(read)
-for j in range(len(nk)):
-    for i in range(len(nk[j])):
-        # opnk[j][i] = round(int(read[0])*math.log10(1+nk[j][i]))
-        opnk[j][i] = round(int(2)*math.log10(1+nk[j][i]))
-print(opnk)
-print(" ")
-# for i in range(len(opnk)):
-# print(*opnk[i])
-img = Image.fromarray(opnk)
-img.save('..\\OutputImages\\resultLogarithmic.png')
+width = len(arr[1])
+height = len(arr)
+filter = 1/9
+for j in range(height):
+    for i in range(width):
+        # constant is 2
+        arr[j][i] = round(
+            int(0.5)*log10(1+arr[j][i])
+        )
+newImg = Image.fromarray(arr)
+newImg.save("..\\OutputImages\\resultLogarithmic2.png")
--- Working/practise.py
+++ Working/practise.py
@@ -1,20 +1,27 @@
 from PIL import Image
 from numpy import array
-img = Image.open("..\\InputImages\\BeforeNegativeTransformation.png")
-arr = array(img)
+img = Image.open(
+    "..\\InputImages\\BeforeNegativeTransformation.png")
+a = img.convert('LA')
+a.show()
+b = img.convert('RGB')
+b.show()
 img.close()
-if arr.ndim > 2:
-    # taking value from 0th index of 3rd dimension i.e r value in case of rgb and take values from other dimensions(1st and 2nd) as it is
-    arr1 = arr[:, :, 0]
-Image.fromarray(arr1).show()
 
-width = len(arr[1])
-height = len(arr)
+# arr = array(img)
+# img.close()
+# if arr.ndim > 2:
+#     # taking value from 0th index of 3rd dimension i.e r value in case of rgb and take values from other dimensions(1st and 2nd) as it is
+#     arr1 = arr[:, :, 0]
+# Image.fromarray(arr1).show()
 
-# converting to grayscale
-for j in range(height):
-    for i in range(width):
-        r, g, b = arr[j][i][0], arr[j][i][1], arr[j][i][2]
-        gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
-        arr[j][i] = [gray, gray, gray, 255]
-Image.fromarray(arr).show()
+# width = len(arr[1])
+# height = len(arr)
+
+# # converting to grayscale
+# for j in range(height):
+#     for i in range(width):
+#         r, g, b = arr[j][i][0], arr[j][i][1], arr[j][i][2]
+#         gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+#         arr[j][i] = [gray, gray, gray, 255]
+# Image.fromarray(arr).show()
